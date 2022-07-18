17493dc5bafe9b64ca12d2a46afebc11f7a8f3c5
sergio.mendez2 <sergio.mendez2@telusinternational.com>
2022-07-18 14:20:55 -0600

    adding extra comments to the project

--- src/main/java/Battalion.java
+++ src/main/java/Battalion.java
@@ -1,6 +1,7 @@
 public class Battalion {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        //extra comments added
         System.out.println("Welcome to the battle of the lord");
         Turn fight = new Turn();
         fight.initBattalion();
